9cf433808b93890633b2cf76784e207847195be8
poorvih01 <poorvih7469@gmail.com>
2026-09-21 16:44:20 +0530

    CRUD sqlachemy

--- sqlalchem.py
+++ sqlalchem.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base,Session
 from sqlalchemy import Column,Integer,String
-from fastapi import FastAPI,Depends
+from fastapi import FastAPI,Depends,HTTPException
 
 app=FastAPI()
 DATABASE_URL="sqlite:///./test.db"
@@ -30,8 +30,66 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
+#CREATE
+@app.post("/todos")
+def create_todo(title:str,db:Session=Depends(get_db)):
+    todo=Todo(title=title,completed="False")#Obj creation
+    db.add(todo)
+    db.commit()
+    db.refresh(todo)
+    return {
+        "message":"Todo created",
+        "data":todo
+    }
+
+#READ ALL DATA
+@app.get("/todos")
+def get_todos(db:Session=Depends(get_db)):
+    todos=db.query(Todo).all()
+    return {
+        "Total":len(todos),
+        "data":todos
+    }
+#READ BY ID
+@app.get("/todos/{todo_id}")
+def get_todo(todo_id:int,db:Session=Depends(get_db)):
+    todo=db.query(Todo).filter(Todo.id==todo_id).first()
+    if not todo:
+        raise HTTPException(status_code=404,detail="Todo not found")
+    return todo
+
+#UPDATE
+@app.put("/todos/{todo_id}")
+def update_todo(todo_id:int,title:str,db:Session=Depends(get_db)):
+    todo=db.query(Todo).filter(Todo.id==todo_id).first()
+    if not todo:
+            raise HTTPException(status_code=404,detail="Todo not found")
+    todo.title=title
+    db.commit()
+    return{
+        "message":"Todo updated",
+        "data":todo
+    }
+
+#DELETE
+@app.delete("/todos/{todo_id}")
+def delete_todo(todo_id:int,db:Session=Depends(get_db)):
+    todo=db.query(Todo).filter(Todo.id==todo_id).first()
+    if not todo:
+        HTTPException(sattus_code=404,detail="Todo not found")
+    db.delete(todo)
+    db.commit()
+    return {
+        "message":"Todo deleted"
+    }
+
+
+
+'''@app.get("/")
 def home(db:Session=Depends(get_db)):
     return {
         "message":"DB connected"
-    }
+    }'''
+
+
+
